305e629866f5767e0a466c545cd000dba41fdd5b
Matthew Boyle <matthewboyle@matthews-mbp.home>
2018-11-10 17:47:32 +0000

    first 'complete' program. Needs way more tests

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/matthewjamesboyle/testEnsure/internal/system"
+import (
+	"fmt"
+	"github.com/matthewjamesboyle/testEnsure/internal/system"
+	"log"
+	"os"
+)
 
 func main() {
-	system.Explore(".")
+	result, err := system.Explore(".")
+	if err != nil {
+		log.Fatalf("Error running program: " + err.Error())
+	}
+	if len(result) == 0 {
+		log.Println("All files are tested!")
+		os.Exit(0)
+	} else {
+		fmt.Println("FAILED: ")
+		fmt.Println("Untested files: ")
+		for _, untested := range result {
+			fmt.Println(untested)
+			os.Exit(-1)
+		}
+	}
 }
