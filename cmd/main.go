00232266f4152e1fd48cfe5370dd971c71b8dbb2
Matthew Boyle <matthewboyle@matthews-mbp.home>
2018-11-11 20:51:23 +0000

    added toml config file reading to project

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"fmt"
+	"github.com/BurntSushi/toml"
 	"github.com/matthewjamesboyle/testEnsure/internal/system"
 	"log"
 	"os"
 )
 
+type skippablepackages struct {
+	Packages []string
+}
+
 func main() {
+	var filesToSkip skippablepackages
+	if _, err := toml.DecodeFile("./testensure.toml", &filesToSkip); err != nil {
+		log.Fatalf(err.Error())
+	}
+
 	result, err := system.Explore(".")
+	fmt.Println(filesToSkip)
 	if err != nil {
 		log.Fatalf("Error running program: " + err.Error())
 	}
+
 	if len(result) == 0 {
 		log.Println("All files are tested!")
 		os.Exit(0)
--- internal/system/explorer_test.go
+++ internal/system/explorer_test.go
@@ -11,9 +11,15 @@ func TestExplore(t *testing.T) {
 		assert.Error(t, err)
 		assert.Nil(t, untest)
 	})
+	t.Run("A directory with no go files returns nil", func(t *testing.T) {
+		untest, err := Explore("../testPack/nongodir")
+		assert.NoError(t, err)
+		assert.Equal(t, 0, len(untest))
+	})
 	t.Run("Correctly returns untested files", func(t *testing.T) {
+		var expectedUntested = []string{"notTested.go", "alsoNotTested.go"}
 		untest, err := Explore("../")
 		assert.NoError(t, err)
-		assert.Equal(t, []string{"notTested.go"}, untest)
+		assert.Equal(t, expectedUntested, untest)
 	})
 }
